e921fd68220e4ee3a50cddd096c637f975ec8b04
Chunrong <56704548+wangchunrong1983@users.noreply.github.com>
2019-11-14 22:02:34 -0800

    assignment completed' dquote

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,59 +1,100 @@
-/*
-	Part 1: We have an array of people's ages, saved in the variable "ages".
-	Write a for-loop that will print each of the elements to the console.
-*/
+
+Part 1
 
 var ages = [28, 30, 17, 14, 12, 20, 13, 36, 68, 49, 37, 55, 56, 26];
+for(var i of ages){
+	console.log(ages[i])
+}
 
-/*
-	Part 2: Let's create a new variable, "younger", which holds an empty array.
-	Using a for-loop, populate the "younger" array with numbers from the "ages"
-	array which are less than or equal to 20.
+Part 2
 
-	Hint: Use the for-loop to go through each element in the "ages" array.
+var younger = [];
 
-	Hint: Use the array.push() method to add an element to the end of an array.
-	(https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/push)
-*/
+for(var i=0; i<ages.length; i++){
+  if(ages[i]<=20){
+  younger.push(ages[i])
+  }
+}
+console.log(younger)
 
-/*
-	Part 3: We're comfortable going through an element from beginning to end, but let's
-	flip this around. With the new "younger" array you created in the last part,
-	print out the elements so that the last element is printed out first, and the
-	first element is printed out last.
+Part 3
+for(var i=younger.length-1;i>=0;i--){
+  console.log(younger[i])
+}
 
-	Hint: What should the index for your for-loop be initialized to if we want to start
-	at the end of the array?
+Part 4
+var i=0;
+while(i<ages.length){
+  if(ages[i]<=20){
+   younger.push(ages[i])
+  }
+   i++;
+}
+var newthing = younger.length-1;
+while(newthing>=0){
+  newthing--;
+}
+console.log(younger)
 
-	Hint: What would the conditional for your for-loop look like?
-*/
 
-/*
-	Part 4: Now that you've finished the first 3 parts using a for-loop, repeat the same exercises
-	(Part 1, Part 2, and Part 3) using a while-loop.
-*/
+Part 5
 
-/*
-	Part 5: Create a function, getDayOfWeek, that will tell you what day of the week it is.
-	For example, getDayOfWeek(1) would return "Monday", and getDayOfWeek(5) would return "Friday".
-	An invalid day of the week should return the string, "Please enter a valid day of the week".
+const getDayOfWeek=(day) => {
 
-	Hint: Using a "switch" statement might result in cleaner code
-*/
+	var dayString = ""
+  
+   switch(day) {
+  
+	case 1: 
+	dayString =  "monday";
+	break;
+  
+	case 2:
+	dayString =  "tuesday";
+	break;
+  
+	case 3:
+	dayString =  "wednesday";
+	break;
+  
+	case 4:
+	dayString =  "thursday";
+	break;
+  
+	case 5:
+	dayString = "friday";
+	break;
+  
+	default:
+	dayString =  "please enter a valid day of week";
+  
+  }
+  return dayString
+  }
+  console.log(getDayOfWeek(5))
 
-/*
-	Bonus - Part 6: All this work is making me hungry. Let's create a new function, addPepperoniToPizza,
-	that accepts an array of pizza ingredients. If the array of ingredients already contains
-	pepperoni, then return the pizza ingredients without doing anything to it. If the array of
-	ingredients does not contain pepperoni, then add pepperoni to the array and return it.
 
-	For example, addPepperoniToPizza(['onions', 'olives', 'pineapple']) should return
-	['onions', 'olives', 'pineapple', 'pepperoni'].
+  Part 6
+  const addPepperoniToPizza = (array) => {
 
-	And addPepperoniToPizza(['spinach', 'pepperoni', 'mushrooms']) should return
-	['spinach', 'pepperoni', 'mushrooms'].
+    if (array.indexOf('pepperoni')===-1){
+      array.push('pepperoni')
+      return array
+    }
+    else{
+      return array;
+    }
+}
 
-	Hint: Take a look at the array.indexOf() method. How can we use this to check if we have pepperoni
-	in our array?
-	(https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/indexOf)
-*/
+
+const addPepperoniToPizza = (array) => {
+
+    if (array.indexOf('pepperoni')===-1){
+      array.push('pepperoni')
+    }
+    return array
+}
+
+
+
+console.log(addPepperoniToPizza(['onion','olive','pineapple']))
