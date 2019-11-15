3c15370361ff5c2a31623864cb9e540fefa17924
kennansalisbury <kennansalisbury@gmail.com>
2019-11-14 21:00:35 -0800

    added solutions to homework

--- starter-code/main.js
+++ starter-code/main.js
@@ -5,6 +5,12 @@
 
 var ages = [28, 30, 17, 14, 12, 20, 13, 36, 68, 49, 37, 55, 56, 26];
 
+console.log("Part 1 Result")
+
+for (let i = 0; i < ages.length; i++) {
+	console.log(ages[i])
+}
+
 /*
 	Part 2: Let's create a new variable, "younger", which holds an empty array.
 	Using a for-loop, populate the "younger" array with numbers from the "ages"
@@ -15,6 +21,18 @@ var ages = [28, 30, 17, 14, 12, 20, 13, 36, 68, 49, 37, 55, 56, 26];
 	Hint: Use the array.push() method to add an element to the end of an array.
 	(https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/push)
 */
+
+console.log("Part 2 Result")
+
+var younger = [];
+
+for (let i = 0; i < ages.length; i++) {
+	if (ages[i] <= 20) {
+		younger.push(ages[i])
+	}
+}
+
+console.log(younger)
 
 /*
 	Part 3: We're comfortable going through an element from beginning to end, but let's
@@ -28,10 +46,44 @@ var ages = [28, 30, 17, 14, 12, 20, 13, 36, 68, 49, 37, 55, 56, 26];
 	Hint: What would the conditional for your for-loop look like?
 */
 
+console.log("Part 3 Result")
+
+for (let i = younger.length - 1; i >= 0; i--) {
+	console.log(younger[i])
+}
+
 /*
 	Part 4: Now that you've finished the first 3 parts using a for-loop, repeat the same exercises
 	(Part 1, Part 2, and Part 3) using a while-loop.
 */
+
+console.log("Part 4 Results")
+console.log("4A")
+let i = 0
+while (i < ages.length) {
+	console.log(ages[i])
+	i++
+}
+
+
+var younger2 = [];
+
+let ind = 0
+while (ind < ages.length) {
+  if (ages[ind] <= 20) {
+	younger2.push(ages[ind])
+  }
+  ind++
+}
+console.log("4B")
+console.log(younger2)
+
+console.log("4C")
+let index = younger2.length - 1
+while (index >= 0) {
+	console.log(younger2[index])
+	index--
+}
 
 /*
 	Part 5: Create a function, getDayOfWeek, that will tell you what day of the week it is.
@@ -40,6 +92,59 @@ var ages = [28, 30, 17, 14, 12, 20, 13, 36, 68, 49, 37, 55, 56, 26];
 
 	Hint: Using a "switch" statement might result in cleaner code
 */
+
+
+console.log("Part 5 - One If Statement Test")
+
+var daysOfWeek = {
+	1: "Monday", 
+	2: "Tuesday",
+	3: "Wednesday",
+	4: "Thursday", 
+	5: "Friday",
+	6: "Saturday",
+	7: "Sunday"
+}
+
+const getDayOfWeek = (num) => {
+	if (daysOfWeek[num]) {
+		console.log(daysOfWeek[num])
+	}
+}
+
+console.log(getDayOfWeek(1))
+
+console.log("Part 5 - Switch Version Test")
+
+const getDayOfWeekSwitch = (num) => {
+	switch(num) {
+		case 1:
+			return "Monday"
+			break
+		case 2:
+			return "Tuesday"
+			break
+		case 3:
+			return "Wednesday"
+			break
+		case 4:
+			return "Thursday"
+			break
+		case 5:
+			return "Friday"
+			break
+		case 6:
+			return "Saturday"
+			break
+		case 7:
+			return "Sunday"
+			break
+		default:
+			return "Please enter a valid day of the week."
+	}
+}
+
+console.log(getDayOfWeekSwitch(1))
 
 /*
 	Bonus - Part 6: All this work is making me hungry. Let's create a new function, addPepperoniToPizza,
@@ -57,3 +162,21 @@ var ages = [28, 30, 17, 14, 12, 20, 13, 36, 68, 49, 37, 55, 56, 26];
 	in our array?
 	(https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/indexOf)
 */
+
+console.log("Part 6 - Bonus")
+
+var ingredientsList1 = ["onions", "olives", "pineapple"]
+var ingredientsList2 = ["spinach", "pepperoni", "mushrooms"]
+
+const addPepperoniToPizza = (arr) => {
+	if (arr.indexOf("pepperoni") === -1) {
+		arr.push("pepperoni")
+		return arr
+	}
+	else {
+		return arr
+	}
+}
+
+console.log(addPepperoniToPizza(ingredientsList1))
+console.log(addPepperoniToPizza(ingredientsList2))
