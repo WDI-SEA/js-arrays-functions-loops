b8d3ab9d95a0010eeebd98bcf605b49246f90306
Durohombredeez7 <Durohombredeez7@gmail.com>
2019-11-15 08:52:23 -0800

    all5inmainjs

--- starter-code/main.js
+++ starter-code/main.js
@@ -5,6 +5,9 @@
 
 var ages = [28, 30, 17, 14, 12, 20, 13, 36, 68, 49, 37, 55, 56, 26];
 
+for (let i = 0; i < ages.length; i++) {
+	console.log(ages[i])
+}
 /*
 	Part 2: Let's create a new variable, "younger", which holds an empty array.
 	Using a for-loop, populate the "younger" array with numbers from the "ages"
@@ -15,6 +18,14 @@ var ages = [28, 30, 17, 14, 12, 20, 13, 36, 68, 49, 37, 55, 56, 26];
 	Hint: Use the array.push() method to add an element to the end of an array.
 	(https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/push)
 */
+var younger = []
+for(let i =0; i < ages.length; i++) {
+	if (ages[i] <= 20) {
+		younger.push(ages[i])
+	} 
+} console.log(younger)
+
+
 
 /*
 	Part 3: We're comfortable going through an element from beginning to end, but let's
@@ -27,11 +38,40 @@ var ages = [28, 30, 17, 14, 12, 20, 13, 36, 68, 49, 37, 55, 56, 26];
 
 	Hint: What would the conditional for your for-loop look like?
 */
+var younger = [17, 14, 12, 20, 13]
+
+for(i = younger.length - 1; i >= 0;i--) {
+	console.log(younger[i])
+}
+
 
 /*
 	Part 4: Now that you've finished the first 3 parts using a for-loop, repeat the same exercises
 	(Part 1, Part 2, and Part 3) using a while-loop.
 */
+var ages = [28, 30, 17, 14, 12, 20, 13, 36, 68, 49, 37, 55, 56, 26];
+
+let i = 0
+while (i < ages.length) {
+	 console.log("AGES: ", ages[i]) 
+	 i++ }
+
+var younger = []
+for (let i = 0; i < ages.length; i++) {
+	while (ages[i] <= 20) {
+		younger.push(ages[i])
+	}
+}
+
+var younger = [17, 14, 12, 20, 13]
+let i = younger.length - 1;
+
+while (i >= 0) {
+	i--;
+	console.log(younger[i])
+}
+
+
 
 /*
 	Part 5: Create a function, getDayOfWeek, that will tell you what day of the week it is.
@@ -40,6 +80,36 @@ var ages = [28, 30, 17, 14, 12, 20, 13, 36, 68, 49, 37, 55, 56, 26];
 
 	Hint: Using a "switch" statement might result in cleaner code
 */
+const getDayOfWeek = (num) => {
+
+	switch (getDayOfWeek) {
+	case 0:
+	day = 'Sunday'
+	break;
+	case 1:
+	day = 'Monday'
+	break;
+	case 2:
+	day = 'Tuesday'
+	break;
+	case 3:
+	day = 'Wednesday'
+	break;
+	case 4: 
+	day = 'Thursday'
+	break;
+	case 5: 
+	day = 'Friday'
+	break;
+	case 6: 
+	day = 'Saturday'
+	break;
+	default:
+	
+	// alert('Please enter a valid day of the week.')
+	}
+}
+getDayOfWeek(3);
 
 /*
 	Bonus - Part 6: All this work is making me hungry. Let's create a new function, addPepperoniToPizza,
